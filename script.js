9a651bfc42189e974d3e6b7998503c7c530433c9
mohammad2003mrz <gorbanben10@gmail.com>
2023-09-08 16:15:41 +0430

    add script

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-'use strict'
+"use strict";
+const btns = document.querySelector(".list-n");
+const btnSubmit = document.querySelector(".btn-s");
+const btnAll = document.querySelectorAll(".btn");
+const front = document.querySelector(".front-card");
+const back = document.querySelector(".back-card");
+
+btns.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("btn")) {
+    btnAll.forEach((btn) => btn.classList.remove("ac-btn"));
+    e.target.classList.add("ac-btn");
+    btnSubmit.addEventListener("click", function (e) {
+      e.preventDefault();
+      front.classList.add("ac-front");
+      back.classList.remove("ac-back");
+    });
+  }
+});
+
+//   if (e.target.classList.contains("btn")) {
+//     btnAll.forEach((btn) => btn.classList.remove("ac-btn"));
+//     e.target.classList.add("ac-btn");
+//     btnSubmit.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       front.classList.add("ac-front");
+//       back.classList.remove("ac-back");
+//     });
+//   }
