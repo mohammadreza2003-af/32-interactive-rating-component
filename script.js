4f39f18e39ff198786e80ad417b897b4e73aa1d3
mohammad2003mrz <gorbanben10@gmail.com>
2023-09-08 16:37:12 +0430

    add btn try

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const btnAll = document.querySelectorAll(".btn");
 const front = document.querySelector(".front-card");
 const back = document.querySelector(".back-card");
 const text = document.querySelector(".text");
+const btnTry = document.querySelector(".btn-t");
 
 btns.addEventListener("click", function (e) {
   e.preventDefault();
@@ -20,7 +21,12 @@ btns.addEventListener("click", function (e) {
     });
   }
 });
-
+btnTry.addEventListener("click", function (e) {
+  e.preventDefault();
+  front.classList.remove("ac-front");
+  back.classList.add("ac-back");
+  btnAll.forEach((btn) => btn.classList.remove("ac-btn"));
+});
 //   if (e.target.classList.contains("btn")) {
 //     btnAll.forEach((btn) => btn.classList.remove("ac-btn"));
 //     e.target.classList.add("ac-btn");
