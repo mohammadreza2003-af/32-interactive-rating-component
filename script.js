1b65d4c9224fa410bc76a2cac7785ce18bc76239
mohammad2003mrz <gorbanben10@gmail.com>
2023-09-08 16:26:23 +0430

    add auto number

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const btnSubmit = document.querySelector(".btn-s");
 const btnAll = document.querySelectorAll(".btn");
 const front = document.querySelector(".front-card");
 const back = document.querySelector(".back-card");
+const text = document.querySelector(".text");
 
 btns.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.classList.contains("btn")) {
     btnAll.forEach((btn) => btn.classList.remove("ac-btn"));
     e.target.classList.add("ac-btn");
+    const data = e.target.getAttribute("data-set");
     btnSubmit.addEventListener("click", function (e) {
       e.preventDefault();
       front.classList.add("ac-front");
       back.classList.remove("ac-back");
+      text.textContent = `You select ${data}`;
     });
   }
 });
